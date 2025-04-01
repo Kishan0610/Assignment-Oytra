b621e2970631aa44793a7742c8bdaf1ee9346b57
Ravikishan Gupta <061002kishan@gmail.com>
2025-04-01 11:43:33 +0530

    deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://file-management-by-kishan.vercel.app',
 });
 
 // Set token from localStorage if it exists
